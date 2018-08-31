df6277dd08e1bc8e8113380fbe5634a079533800
Cliff McCall <cliff.mccall1@gmail.com>
2018-08-31 10:21:15 -0500

    updating

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi I'm a react app</h1>
+        <p>This is really working!</p>
+        <Person name="Max" age="28" />
+        <Person name="Liam" age="4" />
+        <Person name="Amanda" age="28" />
+        <Person name="Cliff" age="34" />
       </div>
     );
     // return React.createElement(
